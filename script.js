d2391ae59eb505267eab940920ac91fc58b2afca
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-05 16:57:59 +0100

    add all

--- script.js
+++ script.js
@@ -14,47 +14,75 @@
     Scott Estrada	  Developer				       scott-estrada-developer.jpg
     Barbara Ramos	  Graphic Designer		     barbara-ramos-graphic-designer.jpg
 
- */
 
+
+  
+ 
+ 
+
+ */
+  const output = document.querySelector('.output');
   const team = [
+
     {
       nome: 'Wayne Barnett',
       ruolo: 'Founder & CEO',
-      foto: 'wayne-barnett-founder-ceo.jpg'
+      foto: "img/wayne-barnett-founder-ceo.jpg"
     },
     {
       nome: 'Angela Caroll ',
       ruolo: 'Chief Editor',
-      foto: 'angela-caroll-chief-editor.jpg'
+      foto: "img/angela-caroll-chief-editor.jpg"
     },
     {
       nome: 'Walter Gordon',
       ruolo: 'Office Manager',
-      foto: 'walter-gordon-office-manager.jpg'
+      foto: "img/angela-lopez-social-media-manager.jpg"
     },
     {
       nome: 'Angela Lopez	',
       ruolo: 'Social Media Manager',
-      foto: 'angela-lopez-social-media-manager.jpg'
+      foto: "img/angela-caroll-chief-editor.jpg"
     },
     {
       nome: 'Scott Estrada',
       ruolo: 'Developer',
-      foto: 'scott-estrada-developer.jpg'
+      foto: "img/scott-estrada-developer.jpg"
     },
-    {
+    {  
       nome: 'Barbara Ramos',
       ruolo: 'Graphic Designer',
-      foto: 'barbara-ramos-graphic-designer.jpg'
+      foto: "img/barbara-ramos-graphic-designer.jpg"
     }
   ]
   console.log(team);
+
+
+  //cosi vado a prendere ogni membro oggetto allinterno dell'array con tutte le loro caratteristiche nome ruolo e foto  
 
   for(let teamMember of team){
     console.log(teamMember.nome);
     console.log(teamMember.ruolo);
     console.log(teamMember.foto);
 
+    output.innerHTML += `
+
+      <div class="col">
+      <div class="card" style="width: 18rem;">
+        <img src="${teamMember.foto}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${teamMember.nome}</h5>
+          <p class="card-text">${teamMember.ruolo}</p>
+        </div>
+      </div>
+    </div>
+    
+    
+    `
+
+
+
   }
+
 
  
